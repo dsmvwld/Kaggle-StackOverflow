3db3100e1e085f612feae0ffd2d0820a611d45c3
Hlg Hrch <heho@gmx.net>
2012-08-25 22:01:03 +0200

    stupid bug fixes, onward to submission 3

--- Makefile
+++ Makefile
@@ -10,7 +10,14 @@ RUBY=ruby
 # uses R 2.14.1 with rf 4.6-6
 R=R
 
-all: submission.csv
+all: submission.csv.gz model.zip
+
+model.zip: Makefile fex.rb predicto.R
+	zip $@ $^
+
+submission.csv.gz: submission.csv
+	cp $< x.csv
+	gzip -9 -f $<
 
 submission.csv: train-sample-f.csv public_leaderboard-f.csv predicto.R
 	$(R) -q --no-restore --no-save <predicto.R
@@ -19,6 +26,6 @@ submission.csv: train-sample-f.csv public_leaderboard-f.csv predicto.R
 	$(RUBY) fex.rb $< $@
 
 clean:
-	rm -f *-f.csv submission.csv
+	rm -f *-f.csv submission.csv model.zip
 
 .PHONY: clean
--- predicto.R
+++ predicto.R
@@ -4,13 +4,13 @@ response <- ts$Status; ts$Status <- NULL
 ids <- ts$PostId; ts$PostId <- NULL
 priors <- c(0.00913, 0.00465, 0.00520, 0.979, 0.00183)
 set.seed(4711)
-# USE classwt=priors, computed dynamically!
-rf <- randomForest(ts, response, ntree=51, do.trace=TRUE)
+rf <- randomForest(ts, response, ntree=71, classwt=priors, do.trace=TRUE)
 # n.b. rf$importance
 #repred <- predict(rf, ts, type="prob")
 #reclassified <- predict(rf, ts, type="response")
 lead <- read.csv("public_leaderboard-f.csv")
-lead$PostId <- NULL
+id <- lead$PostId; lead$PostId <- NULL
 lead$Status <- NULL
 pred <- predict(rf, lead, type="prob")
-write.csv(pred, "submission.csv", quote=FALSE, row.names=FALSE)
+result <- cbind(id, pred)
+write.csv(result, "submission.csv", quote=FALSE, row.names=FALSE)
